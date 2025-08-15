1eb519ebdf26ed8bdf2b6ca044b6cbaf5035db7f
Chiao-Wei Hsu <chiahsu@tesla.com>
2025-08-15 08:01:58 -0700

    vimrc and more aliases

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -4,3 +4,7 @@ HISTFILESIZE=10000
 HISTSIZE=10000
 HISTFILE=~/.bash_history_big
 
+#export PS1=$'\[\e[33m\]\u \[\e[32m\] @\h \[\e[1;36m\] :\W $ \[\e[0;0m\]'
+#export PS1=$'\[\e[33m\]\u \[\e[36m\] @\h \[\e[35m\]:\W $ \[\e[0m\]'
+#export PS1=$'\[\e[32m\] @\h\[\e[1;36m\] :\W $ \[\e[0;0m\]'
+export PS1=$'\[\e[36m\] @\h \[\e[35m\]:\W $ \[\e[0m\]'
