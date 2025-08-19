1e92521337bffee01fccea4b4c461e0cf4b59fa9
Chiao-Wei Hsu <ciaochiaociao@gmail.com>
2025-08-19 01:29:13 -0500

    aichat

--- bash/install_plugins.sh
+++ bash/install_plugins.sh
@@ -1,3 +1,16 @@
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+# Rust / Cargo
+read -p "Install Rust / Cargo? (y/n) " rust
+[[ $rust == "y" ]] && (cargo || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh)
+
+# AIChat
+read -p "Install aichat?" aichat
+if [[ $aichat == "y" ]]; then
+    (
+        source ~/.cargo/env
+        cargo install aichat 
+    )
+fi
